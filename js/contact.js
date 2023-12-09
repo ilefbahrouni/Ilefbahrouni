6c3c7d6b71fdbdf8b2a53205797f42bd7ee7a0ea
ilefbahrouni <148460435+ilefbahrouni@users.noreply.github.com>
2023-12-09 23:57:45 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // You can add your logic here to handle the form submission, like sending an AJAX request.
+
+    alert('Form submitted!'); // For demonstration purposes, replace this with your actual logic.
+});
